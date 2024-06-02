ce79286259d8099ea71ebd2b3561d4d3ee7b3211
Perry Aryee <47823480+perryaryee@users.noreply.github.com>
2024-06-02 18:06:23 +0000

    new_chnages

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { BASEURL, FILEPATH } from '../Connections/BASEURLS';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../Redux/Slices/UserSlice';
-import { Button } from '@mui/material';
+import { Button, IconButton, Toolbar } from '@mui/material';
 
 
 interface video {
@@ -51,14 +51,27 @@ const Home: React.FC = () => {
                                             type="video/mp4" />
 
                                     </video>
-                                    <div className=' px-2 bg-white shadow-sm py-3 lg:rounded-[5px]'>
-                                        <h1 className=' font-semibold text-xl'>{list.title}</h1>
-                                        <h1>{list.description}</h1>
+                                    <div className=' px-2 bg-white shadow-sm py-1 lg:rounded-[5px] flex justify-between items-center'>
+                                        <div>
+                                            <h1 className=' font-semibold text-xl'>{list.title}</h1>
+                                            <h1>{list.description}</h1>
+                                        </div>
+                                        <div>
+                                            <Toolbar title="Share">
+                                                <IconButton>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                                                    </svg>
+                                                </IconButton>
+                                            </Toolbar>
+                                        </div>
                                     </div>
                                     <div className=' grid place-items-center'>
-                                        <div className=' flex items-center space-x-4 mt-3'>
-                                            <button className='  border border-[#703578] bg-white px-4 py-2 rouded-[5px] rounded-[5px] text-[#703578]'>Previous</button>
-                                            <button className=' bg-[#703578] px-8 text-white py-2 rounded-[5px]'>Next</button>
+                                        <div>
+                                            <div className=' flex items-center space-x-4 mt-3'>
+                                                <button className='  border border-[#703578] bg-white px-4 py-2 rouded-[5px] rounded-[5px] text-[#703578]'>Previous</button>
+                                                <button className=' bg-[#703578] px-8 text-white py-2 rounded-[5px]'>Next</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
